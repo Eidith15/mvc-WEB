03edf5a4589abc0df5636ff07d8beec270b14538
Adhitya Sidiq Permana <adhityasp18@gmail.com>
2019-10-07 17:32:44 +0700

    ASSET

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-	<h1>About Me</h1>
+<div class="container">
+	<h1 class="mt-5">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.jpg" alt="Adhitya Sidiq Permana" width="200" class="rounded-circle shadow">
 	<p>Halo, nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun, Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
